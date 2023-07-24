3549bc085d0fc73ddfaf055fb12045e5fc658bb9
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-24 22:20:00 +0800

    gui for button

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from tkinter import *
 #create gui window
 covid_contact_tracing_app = Tk()
 covid_contact_tracing_app.title("COVID-19 CONTACT TRACING APP Version 1.0")
-covid_contact_tracing_app.geometry("1280x720")
+covid_contact_tracing_app.geometry("720x720")
 #make gui for user input
 def covid_gui():
+    gui_label=covid_contact_tracing_app.Label(covid_contact_tracing_app, text='COVID-19 CONTACT TRACING APP', font =( 'Arial', 18))
+    gui_label.pack(padx = 10, pady =10)
     #user's last name
+    userlastname = Label(covid_contact_tracing_app,text = "Last Name : ")
+    covid_contact_tracing_app.grid(row = 0, column = 0,pady=10,padx=5) 
+    e1 = Entry(covid_contact_tracing_app)
+    e1.grid(row = 0, column = 1)
     #user's first name
+    userfirstname = Label(covid_contact_tracing_app,text = "First Name : ") 
+    covid_contact_tracing_app.grid(row = 0, column = 1,pady=10,padx=5)
+    e2 = Entry(covid_contact_tracing_app)
+    e2.grid(row = 0, column = 1)
     #user's middle name
+    usermiddlename = Label(covid_contact_tracing_app,text = "Middle Name : ") 
+    covid_contact_tracing_app.grid(row = 0, column = 2,pady=10,padx=5)
+    e3 = Entry(covid_contact_tracing_app)
+    e3.grid(row = 0, column = 2)
     #user's age
     #user's phone number
     #user's email
@@ -16,4 +30,7 @@ def covid_gui():
     #vaccination status
     #symptoms status
     #contact person details
- 
+    btn = Button(covid_contact_tracing_app, text = "Submit") 
+    btn.grid(row = 3, column = 1)
+#make database
+covid_contact_tracing_app.mainloop()
