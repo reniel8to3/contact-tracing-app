82817e1e66349d2a20cb7609be7295a7d3fbd195
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-24 18:53:24 +0800

    pseudocode

---
+++ main.py
@@ -0,0 +1,15 @@
+#import tkinter
+#create gui window
+#make gui for user input
+    #user's last name
+    #user's first name
+    #user's middle name
+    #user's age
+    #user's phone number
+    #user's email
+    #user's address
+    #vaccination status
+    #symptoms status
+    #contact person details
+ 
+    
