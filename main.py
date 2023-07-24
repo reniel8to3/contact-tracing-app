fc6dc6fde6232f4efdf321e826d360999663bc43
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-24 19:27:07 +0800

    minor revisions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ covid_contact_tracing_app = Tk()
 covid_contact_tracing_app.title("COVID-19 CONTACT TRACING APP Version 1.0")
 covid_contact_tracing_app.geometry("1280x720")
 #make gui for user input
+def covid_gui():
     #user's last name
     #user's first name
     #user's middle name
