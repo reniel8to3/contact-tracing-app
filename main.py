d6ce3b8e1fbfc9a5dc63b5bcd290b7fd06ade05e
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-24 23:09:19 +0800

    revised gui

--- main.py
+++ main.py
@@ -6,31 +6,26 @@ covid_contact_tracing_app.title("COVID-19 CONTACT TRACING APP Version 1.0")
 covid_contact_tracing_app.geometry("720x720")
 #make gui for user input
 def covid_gui():
-    gui_label=covid_contact_tracing_app.Label(covid_contact_tracing_app, text='COVID-19 CONTACT TRACING APP', font =( 'Arial', 18))
+    gui_label=Label(covid_contact_tracing_app, text='COVID-19 CONTACT TRACING APP', font =( 'Arial', 15))
     gui_label.pack(padx = 10, pady =10)
     #user's last name
     userlastname = Label(covid_contact_tracing_app,text = "Last Name : ")
-    covid_contact_tracing_app.grid(row = 0, column = 0,pady=10,padx=5) 
-    e1 = Entry(covid_contact_tracing_app)
-    e1.grid(row = 0, column = 1)
+    userlastname.place(x=25, y=50)
+    userlastname_entry = Entry(covid_contact_tracing_app)
+    userlastname_entry.place(x=105, y=50)
     #user's first name
-    userfirstname = Label(covid_contact_tracing_app,text = "First Name : ") 
-    covid_contact_tracing_app.grid(row = 0, column = 1,pady=10,padx=5)
-    e2 = Entry(covid_contact_tracing_app)
-    e2.grid(row = 0, column = 1)
+    userfirstname = Label(covid_contact_tracing_app,text = "First Name : ")
+    userfirstname.place(x=275, y=50)
+    userfirstname_entry = Entry(covid_contact_tracing_app)
+    userfirstname_entry.place(x=350, y=50)
     #user's middle name
-    usermiddlename = Label(covid_contact_tracing_app,text = "Middle Name : ") 
-    covid_contact_tracing_app.grid(row = 0, column = 2,pady=10,padx=5)
-    e3 = Entry(covid_contact_tracing_app)
-    e3.grid(row = 0, column = 2)
-    #user's age
-    #user's phone number
-    #user's email
-    #user's address
-    #vaccination status
-    #symptoms status
-    #contact person details
-    btn = Button(covid_contact_tracing_app, text = "Submit") 
-    btn.grid(row = 3, column = 1)
+    usermiddlename = Label(covid_contact_tracing_app,text = "Middle Name : ")
+    usermiddlename.place(x=500, y=50)
+    usermiddlename_entry = Entry(covid_contact_tracing_app)
+    usermiddlename_entry.place(x=585, y=50)
 #make database
+
+
+
+covid_gui()
 covid_contact_tracing_app.mainloop()
